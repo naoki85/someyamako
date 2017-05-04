f3dd763058eaf4d9c0f2b11546adafae8bb42245
Naoki Yoneyama <naoki.yoneyama.85@gmail.com>
2017-05-04 20:02:48 +0900

    Test

--- classes/output.py
+++ classes/output.py
@@ -19,6 +19,8 @@ class Output:
         csvlist.append(dora)
         csvlist.append(tile)
 
+        print(csvlist)
+        print(csv_filepath)
         with open(csv_filepath, 'a') as f:
             writer = csv.writer(f, lineterminator='\n')
             writer.writerow(csvlist)
