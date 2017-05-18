a06f4b9eade46013e1e27300b689fcd70ed118fb
Naoki Yoneyama <naoki.yoneyama.85@gmail.com>
2017-05-18 23:59:45 +0900

    Fix environment value

--- config/http_conf.py
+++ config/http_conf.py
@@ -1,7 +1,7 @@
 import os
 from bottle import run
 def bottle_run():
-    if os.getenv("honban_flg"):
+    if os.getenv("HONBAN_FLG"):
         # 本番用の設定
         run(host="163.44.175.59", port=8080, debug=True, reloader=True)
     else:
